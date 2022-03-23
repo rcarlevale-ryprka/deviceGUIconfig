68689fa58618a94477c28a0154b481a451e2fb0e
rcarlevale-ryprka <79945763+rcarlevale-ryprka@users.noreply.github.com>
2022-03-23 15:05:47 -0400

    GUI Additions

--- guiHandle.py
+++ guiHandle.py
@@ -4,6 +4,8 @@
 
 import tkinter
 
+# Defining tkinter module as a variable.
+guiHandle = tkinter.Tk()
 
 #/
 #/ @Classes
@@ -14,19 +16,28 @@ import tkinter
 
 class class_guiHandle:
 
-    #/ @func_guiElements
+    #/ @func_guiHandlePack
+    #/ Purpose: Packs GUI objects and registers them with tkinter.
+    def func_guiHandlePack(guiObject):
+        guiObject.pack()
+
+    #/ @func_guiHandlePrgMenu
     #/ Purpose: 
+    def func_guiHandlePrgMenu():
+        # TEST
+        guiObj_exitButton = tkinter.Button(guiHandle, text = "EXIT", width = 4, height = 2, command = guiHandle.destroy)
+        class_guiHandle.func_guiHandlePack(guiObj_exitButton)
+
 
     #/ @func_guiHandleMain
     #/ Purpose: Main function for handling gui functions.
 
     def func_guiHandleMain(guiPrgTitle):
-
-        # Defining tkinter module as a variable.
-        guiHandle = tkinter.Tk()
-
-        # Defining title of program window.
+        #Defining title of program window.
         guiHandle.title(guiPrgTitle)
+
+        # FILLER
+        class_guiHandle.func_guiHandlePrgMenu()
 
         # Starts GUI and loops it's existence.
         guiHandle.mainloop()
