a23b436e72cc843a6ab8c7b2d494dec3d9bc7d5a
rcarlevale-ryprka <79945763+rcarlevale-ryprka@users.noreply.github.com>
2022-03-23 13:03:05 -0400

    Program Setup

--- debug.py
+++ debug.py
@@ -1,3 +1,13 @@
+#/
+#/ @Classes
+#/ Purpose: Define classes in this file.
+
+#/ @class_debug
+#/ Purpose: Main class for debug.py, this file, holds debug methods.
+
 class class_debug:
+
+    #/ @func_debugOut
+    #/ Purpose: Temporary debug method meant to output a variable.
     def func_debugOut (var1):
         print(var1)
--- main.py
+++ main.py
@@ -27,11 +27,35 @@ import conAPI
 #/ @Classes
 #/ Purpose: Define classes in this file.
 
+#/ @class_main
+#/Purpose: Main class of the file. Meant to call and run the entire program.
+
 class class_main:
 
-    #/ @Define Connection
+    #/ @func_defineConnection
     #/ Purpose: Defines whether the network devices are through a console server or
     #/ are connected locally. If they are connected locally, then it tries to identify the connection  
     #/ or prompt the user.
     def func_defineConnection ():
+
         ...
+
+
+    #/ @func_main
+    #/ Purpose: Main function in the main class.
+
+    def func_main():
+
+        # Hello world.
+        print("Hello World!")
+
+        #Testing debug class & methods.
+        debug.class_debug.func_debugOut("This is a string!")
+        
+
+
+#/
+#/ @Calls
+#/ Purpose: Calling the main class in this program.
+
+class_main.func_main()
