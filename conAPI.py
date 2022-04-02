0d83ee372a560269aaa4e7197046c8ccdafb243e
RCarlevaleSS <79945763+rcarlevale-ryprka@users.noreply.github.com>
2022-04-02 10:29:37 -0400

    Update

--- conAPI.py
+++ conAPI.py
@@ -8,6 +8,8 @@
 import serial
 import time
 
+import debug
+
 
 #/
 #/ @Classes
@@ -17,18 +19,18 @@ import time
 #/ Purpose: Filler purpose. 
 
 class class_connectAPI:
-    #/ @func_connectionPrintOut
-    #/ Purpose: Write to console & debugging function what the output of each command is in a connection with a network device.
-    def func_connectionPrintOut(var_string):
+
+    #/ @func_serialConPrintOut
+    #/ Purpose: Write to console & debugging function what the output of each command is in a connection with a network device if the connection is based through Serial.
+    def func_serialConPrintOut(var_string):
         print(serial.read(str(var_string)).decode("utf-8"), end = "")
 
 
-
     #/ @func_serialConInstance
-    #/ Purpose: 
+    #/ Purpose: Establish a connection through serial to a network device. Allow input and output of commands.
     def func_serialConInstance(arr_cmdInp):
-        #/ Initializes the serial connection.
-        with serial.Serial("COM12", timeout = 9600) as ser:
+        #/ Initializes the serial connection and connects to the serial device based off of the config file.
+        with serial.Serial(debug.func_loadConfig()["conSettings"]["serialConNum"], timeout = debug.func_loadConfig()["conSettings"]["serialComSpeed"]) as ser:
             #/ Establishes serial connection in console.
             print(f"Connecting to {ser.name}...")
 
@@ -38,3 +40,15 @@ class class_connectAPI:
                 time.sleep(0.5)
 
             ser.close()
+
+
+    #/ @func_sshConInstance()
+    #/ Purpose: Establish a connection with an ssh instance to a network device.
+    def func_sshConInstance():
+        ... #FILLER REMOVE WHEN WRITE CODE
+
+
+    #/ @func_sshConInstance()
+    #/ Purpose: Establish a connection with an ssh instance to a network device.
+    def func_telnetConInstance():
+        ... #FILLER REMOVE WHEN WRITE CODE
